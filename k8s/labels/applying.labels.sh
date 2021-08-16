cb7ffa36c50ece968ab7702630c5cb248e76ec91
Thiago L <thiago.lopes.dev@gmail.com>
2021-08-15 23:13:08 -0300

    chore: add script to run kubectl commands

---
+++ k8s/labels/applying.labels.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# author: Thiago Lopes
+# this script create four deployments in kubernetes
+
+# first deploy
+
+kubectl run alpaca-prod --image=gcr.io/kuar-demo/kuard-amd64:blue --replicas=2 --labels="ver=1,app=alpaca,env=prod"
+
+# second deploy
+
+kubectl run alpaca-test --image=gcr.io/kuar-demo/kuard-amd64:green --replicas=1 --labels="ver=2,app=alpaca,env=test"
+
+# third deploy
+
+kubectl run bandicoot-prod --image=gcr.io/kuar-demo/kuard-amd64:green --replicas=2 --labels="ver=2,app=bandicoot,env=prod"
+
+# fourthy deployment
+
+kubectl run bandicoot-staging --image=gcr.io/kuar-demo/kuard-amd64:green --replicas=2 --labels="ver=2,app=bandicoot,env=staging"
