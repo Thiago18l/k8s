4e58a0d4fcd65b850ead971ab7e65f356f109748
Thiago L <thiago.lopes.dev@gmail.com>
2021-08-16 01:20:21 -0300

    fix: removing the flag --replicas in the deployment

--- k8s/labels/applying.labels.sh
+++ k8s/labels/applying.labels.sh
@@ -4,16 +4,16 @@
 
 # first deploy
 
-kubectl run alpaca-prod --image=gcr.io/kuar-demo/kuard-amd64:blue --replicas=2 --labels="ver=1,app=alpaca,env=prod"
+kubectl run alpaca-prod --image=gcr.io/kuar-demo/kuard-amd64:blue --labels="ver=1,app=alpaca,env=prod"
 
 # second deploy
 
-kubectl run alpaca-test --image=gcr.io/kuar-demo/kuard-amd64:green --replicas=1 --labels="ver=2,app=alpaca,env=test"
+kubectl run alpaca-test --image=gcr.io/kuar-demo/kuard-amd64:green --labels="ver=2,app=alpaca,env=test"
 
 # third deploy
 
-kubectl run bandicoot-prod --image=gcr.io/kuar-demo/kuard-amd64:green --replicas=2 --labels="ver=2,app=bandicoot,env=prod"
+kubectl run bandicoot-prod --image=gcr.io/kuar-demo/kuard-amd64:green --labels="ver=2,app=bandicoot,env=prod"
 
 # fourthy deployment
 
-kubectl run bandicoot-staging --image=gcr.io/kuar-demo/kuard-amd64:green --replicas=2 --labels="ver=2,app=bandicoot,env=staging"
+kubectl run bandicoot-staging --image=gcr.io/kuar-demo/kuard-amd64:green --labels="ver=2,app=bandicoot,env=staging"
