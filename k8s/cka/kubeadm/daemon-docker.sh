299c65cdc8632a66941dae6cd7cbfeb68b1d34cc
Thiago L <thiago.lopes.dev@gmail.com>
2021-09-17 01:34:45 -0300

    chore: add additional docker configuration

---
+++ k8s/cka/kubeadm/daemon-docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+# OR
+
+cat > /etc/docker/daemon.json <<EOF 
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+# at the final
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# and restart docker
+
+systemctl daemon-reload
+systemctl restart docker
